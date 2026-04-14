f3f0e6738c2f9decd45e06398bc44be4436aaea1
VaibhavR2004 <vaibhavrajput1977.com>
2026-04-14 12:24:21 +0530

    UC1 commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("""
+---------------------------------------------------------------------
+      WELCOME TO ECO-RIDE URBAN MOBILE SYSTEM
+-----------------------------------------------------------------------""")
+from functions.vehicle import Vehicle,Vehicle_data
+
+
