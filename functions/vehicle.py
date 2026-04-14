7cbcf11a34558b1476d46b68741dc8fada513df2
VaibhavR2004 <vaibhavrajput1977.com>
2026-04-14 16:04:21 +0530

    Vehicle Commit

--- functions/vehicle.py
+++ functions/vehicle.py
@@ -4,3 +4,24 @@ class Vehicle:
         self.vehicle_id=vehicle_id
         self.model=model
         self.battery_percentage= battery_percentage
+
+class Vehicle_data(Vehicle):
+    def __init__(self, vehicle_id, model, battery_percentage,maintenance_status, rental_price):
+        super().__init__(vehicle_id, model, battery_percentage)
+        self.__maintenance_status=maintenance_status
+        self.__rental_price=rental_price
+
+    @property
+    def getter_maintenance(self):
+        return f"{self.vehicle_id} maintenance is {self.__maintenance_status}"
+
+    @property
+    def getter_rental(self):
+        return f"{self.vehicle_id} rental price {self.__rental_price}"
+
+    def getter_batter(self):
+        return f"{self.vehicle_id} battery is {self.battery_percentage}%"
+
+    def setter_battery(self,battery_percentage):
+        self.battery_percentage=battery_percentage if self.battery_percentage<=100 or self.battery_percentage>=0 else None
+
