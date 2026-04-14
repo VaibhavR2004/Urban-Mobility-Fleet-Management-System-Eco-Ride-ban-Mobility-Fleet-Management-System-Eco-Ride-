e740d030d6b9de348bb7953ceccfd44cdb76c0bf
VaibhavR2004 <vaibhavrajput1977.com>
2026-04-14 17:16:49 +0530

    UC5 Done

--- main.py
+++ main.py
@@ -34,8 +34,16 @@ class Electric_Cars(Vehicle):
       def __init__(self, vehicle_id, model, battery_percentage,seating_capacity):
             super().__init__(vehicle_id, model, battery_percentage)
             self.seating_capacity=seating_capacity
-
+      
+      def calculate_trip_cost(self, distance):
+            self.distance=distance
+            total = 5 + 0.5*(self.distance)
+            return f"Total trip coast :{total:.2f}"
 class Scooters(Vehicle):
       def __init__(self, vehicle_id, model, battery_percentage,max_speed_limit):
             super().__init__(vehicle_id, model, battery_percentage)
             self.max_speed_limit=max_speed_limit
+      def calculate_trip_cost(self, time):
+            self.time=time
+            total = 1 + 0.15*(self.time)
+            return f"Total trip coast :{total:.2f}"
