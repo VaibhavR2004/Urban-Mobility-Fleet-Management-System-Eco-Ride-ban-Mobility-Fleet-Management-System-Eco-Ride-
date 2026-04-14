d8607467ce78d87fc1d2dcfb94a1bc6682dc65ec
VaibhavR2004 <vaibhavrajput1977.com>
2026-04-14 16:24:01 +0530

    Use Case 1 done

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ print("""
 ---------------------------------------------------------------------
       WELCOME TO ECO-RIDE URBAN MOBILE SYSTEM
 -----------------------------------------------------------------------""")
-from functions.vehicle import Vehicle,Vehicle_data
+# from functions.vehicle import Vehicle,Vehicle_data
+from abc import ABC, abstractmethod
+class Vehicle:
+     def __init__(self,vehicle_id,model,battery_percentage):
+      self.vehicle_id=vehicle_id
+      self.model=model
+      self.battery_percentage= battery_percentage
 
 
